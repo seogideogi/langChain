a6477797530a14b364aaa736f06e9ef80f3d3fab
seogideogi <rhodalee78@gmail.com>
2025-01-11 18:35:21 +0900

    Add files via upload

---
+++ rankup_PDA.py
@@ -0,0 +1,144 @@
+import os
+import streamlit as st
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.vectorstores import Chroma
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories.streamlit import StreamlitChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+
+
+## 6.5 에서 의존성 문제 괸련 추가 코드
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+#오픈AI API 키 설정
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
+#cache_resource로 한번 실행한 결과 캐싱해두기
+@st.cache_resource
+def load_and_split_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    return loader.load_and_split()
+
+#텍스트 청크들을 Chroma 안에 임베딩 벡터로 저장
+@st.cache_resource
+def create_vector_store(_docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    split_docs = text_splitter.split_documents(_docs)
+    persist_directory = "./chroma_db"
+    vectorstore = Chroma.from_documents(
+        split_docs, 
+        OpenAIEmbeddings(model='text-embedding-3-small'),
+        persist_directory=persist_directory
+    )
+    return vectorstore
+
+#만약 기존에 저장해둔 ChromaDB가 있는 경우, 이를 로드
+@st.cache_resource
+def get_vectorstore(_docs):
+    persist_directory = "./chroma_db"
+    if os.path.exists(persist_directory):
+        return Chroma(
+            persist_directory=persist_directory,
+            embedding_function=OpenAIEmbeddings(model='text-embedding-3-small')
+        )
+    else:
+        return create_vector_store(_docs)
+    
+# PDF 문서 로드-벡터 DB 저장-검색기-히스토리 모두 합친 Chain 구축  
+# 메모리 기능이 추가된 RAG
+# 사용자와 AI간의 대화를 맥락화 하도록 명령 하는 프롬프트 선언 역할. 주어진 채팅 히스토리와 사용자의 마지막 질문을 바탕으로 하나의 독립된 질문을 구성 하라는 의미
+
+@st.cache_resource
+def initialize_components(selected_model):
+    #file_path = r"C:\Users\lee.eunjeong\Downloads\대한민국헌법(헌법)(제00010호)(19880225).pdf"
+    file_path = r"./개인채무조정.pdf"
+    pages = load_and_split_pdf(file_path)
+    vectorstore = get_vectorstore(pages)
+    retriever = vectorstore.as_retriever()
+
+    # 채팅 히스토리 요약 시스템 프롬프트
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    # 질문-답변 시스템 프롬프트
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say that you don't know. \
+    Keep the answer perfect. please use imogi with the answer.
+    대답은 한국어로 하고, 존댓말을 써줘.\
+
+    {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("history"),
+            ("human", "{input}"),
+        ]
+    )
+
+	# 앞서 선언한 요소들을 하나의 체인에 묶기 위해 아래와 같은 과정 진행
+    llm = ChatOpenAI(model=selected_model)
+    history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+	
+    return rag_chain
+
+# Streamlit UI
+
+st.header("개인 채무 조정 Q&A 챗봇 💬 📚")
+option = st.selectbox("Select GPT Model", ("gpt-4o-mini", "gpt-3.5-turbo-0125"))
+rag_chain = initialize_components(option)
+chat_history = StreamlitChatMessageHistory(key="chat_messages") # 스트림릿 상에서 사용자와 AI가 주고받은 대화가 Chat_messages라는 key값의 value로 저장 됨
+
+# 지속되는 채킹 히스토리 활용을 위해 RunnableWithMessageHistory 선언하고 활용
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    lambda session_id: chat_history,
+    input_messages_key="input",
+    history_messages_key="history",
+    output_messages_key="answer",
+)
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", 
+                                     "content": "개인 채무조정 관련, 무엇이든 물어보세요!"}]
+
+for msg in chat_history.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+
+# st.chat_input 인풋박스
+if prompt_message := st.chat_input("Your question"):  
+    st.chat_message("human").write(prompt_message)
+    with st.chat_message("ai"):
+        with st.spinner("Thinking..."):
+            config = {"configurable": {"session_id": "any"}}
+            response = conversational_rag_chain.invoke(
+                {"input": prompt_message},
+                config) # AI의 응답을 받고
+            
+            answer = response['answer']
+            st.write(answer)
+            with st.expander("참고 문서 확인"):
+                for doc in response['context']:
+                    st.markdown(doc.metadata['source'], help=doc.page_content)
